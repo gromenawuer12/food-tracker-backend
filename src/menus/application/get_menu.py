d466a35e047e0d93d5559788b1a1021ea1ee0ba7
Gromenawuer <gromenawuer12@gmail.com>
2023-05-09 16:52:36 +0200

    fixing menus

--- src/menus/application/get_menu.py
+++ src/menus/application/get_menu.py
@@ -8,14 +8,11 @@ class GetMenu:
         self.__database = database
 
     def execute(self, user, fromDate, toDate):
-        if user is None:
-            if fromDate is None or toDate is None:
+        if fromDate is None or toDate is None:
                 response = []
-            else:
-                response = self.__database.findByDate(fromDate, toDate)
         else:
-            if fromDate is None or toDate is None:
-                response = self.__database.findByUser(user)
+            if user is None:
+                response = self.__database.findByDate(user)
             else:
                 response = self.__database.find(user, fromDate, toDate)
         return response
