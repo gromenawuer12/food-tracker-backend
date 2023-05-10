3e47cb70593a05bc512fa62a8d30905ba88a1f4f
Gromenawuer <gromenawuer12@gmail.com>
2023-05-10 14:59:40 +0200

    fix menus

--- src/menus/infrastructure/database/menu_dynamodb.py
+++ src/menus/infrastructure/database/menu_dynamodb.py
@@ -31,12 +31,12 @@ class MenuDynamoDB(MenuDatabase):
         if ',' in user:
             user_list = user.split(',')
             user_list_parsed = ' OR PK = menu#'.join(user_list)
-            user_list_parsed = 'PK = menu#' + user_list_parsed
+            user_list_parsed = '(PK = menu#' + user_list_parsed
         else:
-            user_list_parsed = 'PK = ' + user
+            user_list_parsed = '(PK = ' + user + ''
         print(user_list_parsed)
         response = self.table.query(
-            KeyConditionExpression=user_list_parsed + ' AND #sk BETWEEN :start_date AND :end_date',
+            KeyConditionExpression=user_list_parsed + ') AND #sk BETWEEN :start_date AND :end_date',
             ExpressionAttributeNames={
                 '#sk': 'SK',
                 '#dt': 'date'
